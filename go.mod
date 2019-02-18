0ad289b5bed905c89c36e0ca91a2b967ad5808c6
77anandkumar <77anandkumar@gmail.com>
2019-02-18 18:11:01 +0900

    Implement getrout api

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/tempo-nksn/Tempo-Backend
 require (
 	github.com/gin-contrib/sse v0.0.0-20190125020943-a7658810eb74 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/google/uuid v1.1.0 // indirect
+	github.com/kr/pretty v0.1.0
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/onsi/ginkgo v1.7.0
 	github.com/onsi/gomega v1.4.3
 	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
+	golang.org/x/time v0.0.0-20181108054448-85acf8d2951c // indirect
+	googlemaps.github.io/maps v0.0.0-20190206003505-be134e760d70
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- server/handlers.go
+++ server/handlers.go
@@ -1,13 +1,24 @@
 package server
 
 import (
+	"context"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/tempo-nksn/Tempo-Backend/models"
+	"log"
 	"math/rand"
 	"net/url"
+	"os"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kr/pretty"
+	"github.com/tempo-nksn/Tempo-Backend/models"
+	"googlemaps.github.io/maps"
 )
+
+type places struct {
+	origin      string
+	destination string
+}
 
 func hello(c *gin.Context) {
 	c.String(200, "Hello User, your taxi is booked")
@@ -30,7 +41,7 @@ func getNearByTaxis(c *gin.Context) {
 	var userLocation models.Location
 
 	r := c.Request
-	m,_ := url.ParseQuery(r.URL.RawQuery)
+	m, _ := url.ParseQuery(r.URL.RawQuery)
 
 	if _, ok := m["latitude"]; !ok {
 		c.JSON(400, "User Location Missing!!!!!")
@@ -42,21 +53,71 @@ func getNearByTaxis(c *gin.Context) {
 	}
 	userLocation.Latitude = m["latitude"][0]
 	userLocation.Longitude = m["longitude"][0]
-	latitude,_ := strconv.ParseFloat(userLocation.Latitude, 64)
-	longitude,_ := strconv.ParseFloat(userLocation.Longitude, 64)
+	latitude, _ := strconv.ParseFloat(userLocation.Latitude, 64)
+	longitude, _ := strconv.ParseFloat(userLocation.Longitude, 64)
 
-	fmt.Printf("%f",latitude)
-	fmt.Printf("%f",longitude)
+	fmt.Printf("%f", latitude)
+	fmt.Printf("%f", longitude)
 
 	//c.JSON(200, userLocation)
-	var numberOfTaxis  = 3
+	var numberOfTaxis = 3
 	var taxiLocation = make([]models.Location, numberOfTaxis)
-	for count := 0; count<numberOfTaxis; count++ {
-		taxiLocation[count].Latitude = fmt.Sprintf("%0.6f",(latitude + float64(rand.Intn(5000) - 2500)/100000))
-		taxiLocation[count].Longitude = fmt.Sprintf("%0.6f",(longitude + float64(rand.Intn(5000) - 2500)/100000))
+	for count := 0; count < numberOfTaxis; count++ {
+		taxiLocation[count].Latitude = fmt.Sprintf("%0.6f", (latitude + float64(rand.Intn(5000)-2500)/100000))
+		taxiLocation[count].Longitude = fmt.Sprintf("%0.6f", (longitude + float64(rand.Intn(5000)-2500)/100000))
 	}
 
 	c.JSON(200, taxiLocation)
 
 }
 
+func getPolyLine(c *gin.Context) {
+
+	r := c.Request
+	m, _ := url.ParseQuery(r.URL.RawQuery)
+
+	if _, ok := m["src"]; !ok {
+		c.JSON(400, "user source Missing!!!!!")
+		return
+	}
+	if _, ok := m["dest"]; !ok {
+		c.JSON(400, "User destination Missing!!!!!")
+		return
+	}
+
+	srcAndDest := places{origin: m["src"][0], destination: m["dest"][0]}
+	allPolyline := getRoute(srcAndDest)
+	c.JSON(200, allPolyline)
+}
+
+func getRoute(obj places) []string {
+	googleKey := os.Getenv("MAPS_KEY")
+	c, err := maps.NewClient(maps.WithAPIKey(googleKey))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	r := &maps.DirectionsRequest{
+		Origin:      obj.origin,
+		Destination: obj.destination,
+	}
+	r.Mode = maps.TravelModeDriving
+	r.Units = maps.UnitsMetric
+	route, _, err := c.Directions(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	var maplegs *maps.Leg
+	maplegs = route[0].Legs[0]
+	pretty.Println(maplegs)
+	var allPolyline []string
+	var polyline string
+
+	for i := 0; i < len(maplegs.Steps); i++ {
+
+		polyline = maplegs.Steps[i].Polyline.Points
+		allPolyline = append(allPolyline, polyline)
+	}
+	return allPolyline
+
+}
