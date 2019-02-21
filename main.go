9010ff826618752b9b127ed4da220767b63e6a2c
prashant maurya and Manas Agarwal <mauryaprashant.1992+manasagarwal96@gmail.com>
2019-02-21 10:53:30 +0900

    configuring environment variables

--- main.go
+++ main.go
@@ -6,13 +6,25 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/joho/godotenv"
 	"github.com/tempo-nksn/Tempo-Backend/db"
 	"github.com/tempo-nksn/Tempo-Backend/server"
 )
 
 func main() {
 	// Connecting to the database
-	database, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	DATABASE := os.Getenv("DB_DRIVER")
+	databaseURL := os.Getenv("DATABASE_URL")
+	if DATABASE == "" && databaseURL == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		DATABASE = os.Getenv("DB_DRIVER")
+		databaseURL = os.Getenv("DATABASE_URL")
+	}
+	database, err := gorm.Open(DATABASE, databaseURL)
 	if err != nil {
 		log.Fatal(err)
 		panic("failed to establish database connection")
@@ -23,4 +35,5 @@ func main() {
 	// Creating a router
 	router := server.CreateRouter(database)
 	server.StartServer(router)
+
 }
