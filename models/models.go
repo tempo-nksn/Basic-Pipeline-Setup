78ea6bef6fe2e2ba3ce7f93004ab56696158fb8e
subodh101 <subodh.pushkar@gmail.com>
2019-02-24 22:57:15 +0900

    Previous Fix

--- models/models.go
+++ models/models.go
@@ -34,6 +34,7 @@ type Taxi struct {
 	Capacity               int
 	CurrentNumOfTravellers int
 	Status                 string // Can be one of Free, Active, Full
+	DriverID    uint
 }
 
 type GooglePath struct {
@@ -52,7 +53,6 @@ type Route struct {
 	Source      string
 	Destination string
 	Status      string // can be Active or Passive
-	DriverID    uint
 }
 
 type Booking struct {
